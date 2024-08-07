aeb551155ac1111756af3f54037aa9796e1a94cd
heatherbrain <z.athifa08@gmail.com>
2024-08-07 20:24:52 +0530

    adding new TextInput

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -10,17 +10,45 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _amountController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding:  EdgeInsets.all(16),
+    return Padding(
+      padding: EdgeInsets.all(16),
       child: Column(
         children: [
           TextField(
+            controller: _titleController,
             maxLength: 55,
-            decoration: InputDecoration(
-              label: Text('title')
-            ),
+            decoration: const InputDecoration(label: Text('title')),
+          ),
+          TextField(
+            controller: _amountController,
+            keyboardType: TextInputType.number,
+            decoration: const InputDecoration(
+              prefixText: '\$',
+              label: Text('amount'),
+              ),
+          ),
+          Row(
+            children: [
+              TextButton(onPressed: () {}, child: const Text('Cancel'),),
+              ElevatedButton(
+                  onPressed: () {
+                    print(_titleController.text);
+                    print(_amountController.text);
+                  },
+                  child: const Text('Save Expense'))
+            ],
           ),
         ],
       ),
